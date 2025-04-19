cde368d1999fdd222c0b65d1f100b1612583229f
vantanb1k55 <vantanb1k55@gmail.com>
2025-04-19 15:47:32 +0700

    thêm title cho game grid

--- src/pages/Home/gameGrid/gameGrid.tsx
+++ src/pages/Home/gameGrid/gameGrid.tsx
@@ -16,63 +16,76 @@ export function GameGridComponent({ games }: { games: GameItem[] }) {
   const mobileCols = [games.slice(0, 6), games.slice(6)];
 
   return (
-    <div className="flex px-4 max-w-7xl mx-auto py-14">
-      {/* MOBILE: 2 CỘT */}
-      <div className="flex flex-row gap-4 w-full lg:hidden">
-        {mobileCols.map((col, colIndex) => (
-          <div
-            key={colIndex}
-            className={`flex-1 flex flex-col gap-6 ${
-              colIndex === 1 ? "mt-10" : ""
-            }`}
-          >
-            {col.map((game, idx) => (
-              <div
-                key={idx}
-                className="bg-white rounded-xl shadow-md overflow-hidden relative"
-              >
-                <img
-                  src={game.image}
-                  alt={game.title}
-                  className="w-full h-auto object-cover"
-                />
-                <div className="p-3 absolute bottom-0 text-white bg-gradient-to-t from-purple-500/10 via-gray-500/10  to-gray-500/5">
-                  <h2 className="text-lg font-semibold mb-2">{game.title}</h2>
-                  <p className="text-[10px] ">{game.description}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        ))}
+    <div className=" px-4 max-w-7xl mx-auto py-14">
+      <div className="flex flex-col justify-center items-center pb-10 gap-4 px-32">
+        <h1 className="text-6xl font-bold">Our Games</h1>
+        <span className="text-center text-sm text-gray-500">
+          As a pioneer of mobile app gamification, we take pride in originality
+          and individuality, providing global players with state-of-the-art
+          games that feature splendid storylines, sensational sound effects and
+          magnificent animation that never cease to impress.
+        </span>
       </div>
-
-      {/* DESKTOP: 4 CỘT */}
-      <div className="hidden lg:flex flex-row gap-6 w-full">
-        {desktopCols.map((col, colIndex) => (
-          <div
-            key={colIndex}
-            className={`flex-1 flex flex-col gap-6
-              ${colIndex === 1 || colIndex === 3 ? "mt-14" : ""}
-            `}
-          >
-            {col.map((game, idx) => (
-              <div
-                key={idx}
-                className="bg-white rounded-xl shadow-md overflow-hidden relative"
-              >
-                <img
-                  src={game.image}
-                  alt={game.title}
-                  className="w-full h-auto object-cover hover:scale-105 transition-transform duration-300 cursor-pointer"
-                />
-                <div className="p-8 absolute bottom-0 text-white bg-gradient-to-t from-purple-500/10 via-gray-500/10  to-gray-500/5">
-                  <h2 className="text-3xl font-semibold mb-2">{game.title}</h2>
-                  <p className="text-xs ">{game.description}</p>
+      <div className="flex ">
+        {/* MOBILE: 2 CỘT */}
+        <div className="flex flex-row gap-4 w-full lg:hidden">
+          {mobileCols.map((col, colIndex) => (
+            <div
+              key={colIndex}
+              className={`flex-1 flex flex-col gap-6 ${
+                colIndex === 1 ? "mt-10" : ""
+              }`}
+            >
+              {col.map((game, idx) => (
+                <div
+                  key={idx}
+                  className="bg-white rounded-xl shadow-md overflow-hidden relative"
+                >
+                  <img
+                    src={game.image}
+                    alt={game.title}
+                    className="w-full h-auto object-cover"
+                  />
+                  <div className="p-3 absolute bottom-0 text-white bg-gradient-to-t from-purple-500/10 via-gray-500/10  to-gray-500/5">
+                    <h2 className="text-lg font-semibold mb-2">{game.title}</h2>
+                    <p className="text-[10px] ">{game.description}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        ))}
+              ))}
+            </div>
+          ))}
+        </div>
+
+        {/* DESKTOP: 4 CỘT */}
+        <div className="hidden lg:flex flex-row gap-6 w-full">
+          {desktopCols.map((col, colIndex) => (
+            <div
+              key={colIndex}
+              className={`flex-1 flex flex-col gap-6
+             ${colIndex === 1 || colIndex === 3 ? "mt-14" : ""}
+           `}
+            >
+              {col.map((game, idx) => (
+                <div
+                  key={idx}
+                  className="bg-white rounded-xl shadow-md overflow-hidden relative"
+                >
+                  <img
+                    src={game.image}
+                    alt={game.title}
+                    className="w-full h-auto object-cover hover:scale-105 transition-transform duration-300 cursor-pointer"
+                  />
+                  <div className="p-8 absolute bottom-0 text-white bg-gradient-to-t from-purple-500/10 via-gray-500/10  to-gray-500/5">
+                    <h2 className="text-3xl font-semibold mb-2">
+                      {game.title}
+                    </h2>
+                    <p className="text-xs ">{game.description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
