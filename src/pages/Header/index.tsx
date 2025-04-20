aa2be6535e9642a52840fc39884a9647de296c78
vantanb1k55 <vantanb1k55@gmail.com>
2025-04-20 09:41:19 +0700

    dieu chinh giao dien dong ho

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -1,6 +1,5 @@
 import CountdownTimer from "./countdown";
 
-
 export function Header() {
   const now = new Date();
   const targetDate = new Date(
@@ -9,10 +8,36 @@ export function Header() {
 
   return (
     <div className="relative">
-      <div className=" flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
-        <CountdownTimer targetDate={targetDate} />
+      <div className=" flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  gap-y-14  text-white  bg-[radial-gradient(circle,_rgba(168,85,247,0.3)_0%,_rgba(168,85,247,0)_90%)] shadow-2xl shadow-white/5 md:p-12 rounded-full">
+        <div className="text-center">
+          <div className="font-black text-[40px] md:text-[80px] font-playfair text-center px-4 pb-4">
+            <h1>We’re Getting Ready</h1>
+          </div>
+          <CountdownTimer targetDate={targetDate} />
+        </div>
+        <div className="flex flex-col items-center justify-center text-center w-[350px] md:w-96 ">
+          <p className="text-xs md:text-lg]">
+            We will back to something amazing. Getting the latest updates about
+            our games. Please sign up to our newsletter.
+          </p>
+          <div className="flex items-center bg-white text-gray-700 rounded-md mt-5 w-full">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="py-3 px-4 w-full focus:outline-none bg-transparent "
+            />
+            <button className="  rounded-r-md px-5 ">
+              <img src="./icons/arrow-right-black.png" alt="" />
+            </button>
+          </div>
+        </div>
       </div>
-      <img src="./images/BG_header.png" alt="" className="w-full h-full" />
+
+      <img
+        src="./images/BG_header.png"
+        alt=""
+        className=" md:w-full h-[520px] w-auto object-cover md:h-full"
+      />
     </div>
   );
 }
